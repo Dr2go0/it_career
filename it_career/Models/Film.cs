ee60f0eacfafa503e09ef65a6d2f92f39fce137f
Alex <drago5998100@gmail.com>
2026-03-20 09:25:55 +0200

    create classes Kino and Filkm

---
+++ it_career/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace it_career.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ it_career/Models/Kino.cs
@@ -0,0 +1,10 @@
+﻿namespace it_career.Models
+{
+    public class Kino
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+    }
+}
