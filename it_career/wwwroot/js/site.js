d68447d43b867ad52f20c17abd7ce9bdc3d51268
Alex <drago5998100@gmail.com>
2026-03-25 21:30:06 +0200

    create only one kinoForm

--- it_career/wwwroot/js/site.js
+++ it_career/wwwroot/js/site.js
@@ -3,52 +3,54 @@
 
 // Write your JavaScript code.
 function CreatKino() {
+    if (!document.getElementById("kinoForm")) {
+        const form = document.createElement("form");
+        form.className = "form-group m-2 p-2 d-flex flex-column align-items-start";
+        form.id = "kinoForm";
+        form.method = "POST";
+        form.action = "/Home/SaveKino";
 
-    const form = document.createElement("form");
-    form.className = "form-group m-2 p-2 d-flex flex-column align-items-start";
-    form.method = "POST";
-    form.action = "/Home/SaveKino";
+        const btnClose = document.createElement("button");
+        btnClose.className = "btn btn-close m-2";
+        btnClose.addEventListener("click", function (event) {
+            form.remove();
+        });
 
-    const btnClose = document.createElement("button");
-    btnClose.className = "btn btn-close m-2";
-    btnClose.addEventListener("click", function(event){
-        form.remove();
-    });
+        const btnCreate = document.createElement("button");
+        btnCreate.className = "btn btn-primary m-2";
+        btnCreate.type = "submit";
+        btnCreate.innerHTML = "Submit";
+        btnCreate.addEventListener("click", function (event) {
+            form.submit();
+        });
 
-    const btnCreate = document.createElement("button");
-    btnCreate.className = "btn btn-primary m-2";
-    btnCreate.type = "submit";
-    btnCreate.innerHTML = "Submit";
-    btnCreate.addEventListener("click", function (event) {
-        form.submit();
-    });
+        const inputName = document.createElement("input");
+        inputName.type = "text";
+        inputName.name = "Name";
+        inputName.placeholder = "Name";
+        inputName.className = "form-control m-2";
+        inputName.autocomplete = "name webauthn";
 
-    const inputName = document.createElement("input");
-    inputName.type = "text";
-    inputName.name = "Name";
-    inputName.placeholder = "Name";
-    inputName.className = "form-control m-2";
-    inputName.autocomplete = "name webauthn";
+        const inputLocation = document.createElement("input");
+        inputLocation.type = "text";
+        inputLocation.name = "Location";
+        inputLocation.placeholder = "Location";
+        inputLocation.className = "form-control m-2";
 
-    const inputLocation = document.createElement("input");
-    inputLocation.type = "text";
-    inputLocation.name = "Location";
-    inputLocation.placeholder = "Location";
-    inputLocation.className = "form-control m-2";
+        const inputCapacity = document.createElement("input");
+        inputCapacity.type = "number";
+        inputCapacity.name = "Capacity";
+        inputCapacity.placeholder = "Capacity";
+        inputCapacity.className = "form-control m-2";
 
-    const inputCapacity = document.createElement("input");
-    inputCapacity.type = "number";
-    inputCapacity.name = "Capacity";
-    inputCapacity.placeholder = "Capacity";
-    inputCapacity.className = "form-control m-2";
+        document.getElementById("formContainer").appendChild(form);
 
-    document.getElementById("formContainer").appendChild(form);
-
-    form.appendChild(btnClose);
-    form.appendChild(inputName);
-    form.appendChild(inputLocation);
-    form.appendChild(inputCapacity);
-    form.appendChild(btnCreate);
+        form.appendChild(btnClose);
+        form.appendChild(inputName);
+        form.appendChild(inputLocation);
+        form.appendChild(inputCapacity);
+        form.appendChild(btnCreate);
+    }
 }
 
 function KinoSchedule(kinoName) {
