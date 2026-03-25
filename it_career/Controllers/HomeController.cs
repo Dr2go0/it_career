361fd64ee4eae361911fd59f676b774efa4b457f
Alex <drago5998100@gmail.com>
2026-03-25 20:31:03 +0200

    Kino db logic

--- it_career/Controllers/HomeController.cs
+++ it_career/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-using System.Diagnostics;
+using Humanizer;
+using it_career.data.models;
 using it_career.infrastructure.Interface;
 using it_career.models;
 using it_career.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace it_career.Controllers
 {
@@ -10,6 +13,7 @@ namespace it_career.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IUserRepository _userRepository;
+        private readonly IKinoRepository _kinoRepository;
         public HomeController(ILogger<HomeController> logger, IUserRepository userRepository)
         {
             _logger = logger;
@@ -19,7 +23,13 @@ namespace it_career.Controllers
         
         public IActionResult Index()
         {
-            return View();
+            //List<KinoDto> kinos = _kinoRepository.GetAll<KinoDto>().ToList();
+            List<KinoDto> kinos = new List<KinoDto>();
+                KinoDto kino1 = new KinoDto() { Name = "Kino1", Location = "Location1", Capacity = 100 };
+                KinoDto kino2 = new KinoDto() { Name = "Kino2", Location = "Location2", Capacity = 150 };
+                kinos.Add(kino1);
+                kinos.Add(kino2);
+            return View(kinos);
         }
 
         public IActionResult Privacy()
@@ -34,10 +44,18 @@ namespace it_career.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         [HttpPost]
-        public IActionResult SaveKino(KinoDto kino)
+        public IActionResult SaveKino(KinoDto kinoDto)
         {
-            
-            return View();
+            var kino = new Kino
+            {
+                Name = kinoDto.Name,
+                Location=kinoDto.Location,
+                Capacity = kinoDto.Capacity
+            };
+
+            //_kinoRepository.Kinos.Add(kino);
+            _kinoRepository.Save();
+            return RedirectToAction("Index");
         }
     }
 }
--- it_career/wwwroot/js/site.js
+++ it_career/wwwroot/js/site.js
@@ -2,59 +2,60 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
 function CreatKino() {
 
     const form = document.createElement("form");
-    form.ClassName = "form-group";
-    form.ClassName = "horizontal";
+    form.className = "form-group m-2 p-2 d-flex flex-column align-items-start";
     form.method = "POST";
-
     form.action = "/Home/SaveKino";
 
     const btnClose = document.createElement("button");
-    btnClose.ClassName = "btn-close";
-    btnClose.innerHTML = "X";
+    btnClose.className = "btn btn-close m-2";
+    btnClose.addEventListener("click", function(event){
+        form.remove();
+    });
 
     const btnCreate = document.createElement("button");
-    btnCreate.ClassName = "btn btn-primary";
+    btnCreate.className = "btn btn-primary m-2";
     btnCreate.type = "submit";
     btnCreate.innerHTML = "Submit";
-
-    const btnSchedules = document.createElement("button");
-    btnSchedules.ClassName = "btn btn-secondary";
-    btnSchedules.type = "button";
-    btnSchedules.innerHTML = "Schedules";
     btnCreate.addEventListener("click", function (event) {
         form.submit();
-        document.getElementById("kinosContainer").InnerHTML = "<p> Name:" + form.elements["Name"].value + "</p><p> Location:" + from.elements["Location"].value + "</p>";
-        document.getElementById("kinosContainer").appendChild(btnSchedules);
-        form.reset();
     });
 
     const inputName = document.createElement("input");
     inputName.type = "text";
     inputName.name = "Name";
     inputName.placeholder = "Name";
-    inputName.className = "form-control";
+    inputName.className = "form-control m-2";
+    inputName.autocomplete = "name webauthn";
 
     const inputLocation = document.createElement("input");
     inputLocation.type = "text";
     inputLocation.name = "Location";
     inputLocation.placeholder = "Location";
-    inputLocation.className = "form-control";
+    inputLocation.className = "form-control m-2";
 
     const inputCapacity = document.createElement("input");
     inputCapacity.type = "number";
     inputCapacity.name = "Capacity";
     inputCapacity.placeholder = "Capacity";
-    inputCapacity.className = "form-control";
+    inputCapacity.className = "form-control m-2";
 
-    document.getElementById("kinosContainer").appendChild(form);
+    document.getElementById("formContainer").appendChild(form);
 
     form.appendChild(btnClose);
     form.appendChild(inputName);
     form.appendChild(inputLocation);
     form.appendChild(inputCapacity);
     form.appendChild(btnCreate);
+}
+
+function KinoSchedule() {
+    const form = document.createElement("form");
+    form.className = "form-group";
+    form.className = "horizontal";
+    form.className = "flex-grow-1";
+    form.method = "POST";
+    form.action = "/Home/SaveSchedule";
 }
