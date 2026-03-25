d1cc883870870302c3d49de96f2cc9ff5e42ac91
Dr2go0 <drago5998100@gmail.com>
2026-03-25 13:35:51 +0200

    Kino data from view to Controller

--- it_career/wwwroot/js/site.js
+++ it_career/wwwroot/js/site.js
@@ -4,39 +4,57 @@
 // Write your JavaScript code.
 
 function CreatKino() {
-    const form = document.Create.createElement("form");
+
+    const form = document.createElement("form");
     form.ClassName = "form-group";
     form.ClassName = "horizontal";
+    form.method = "POST";
 
-    const btnClose = document.Create.createElement("button");
+    form.action = "/Home/SaveKino";
+
+    const btnClose = document.createElement("button");
     btnClose.ClassName = "btn-close";
-    btnCLose.AddEventListener("click", function (event) {
-        form.remove();
-    });
+    btnClose.innerHTML = "X";
 
-    const btnCreate = document.Create.createElement("button");
+    const btnCreate = document.createElement("button");
     btnCreate.ClassName = "btn btn-primary";
     btnCreate.type = "submit";
     btnCreate.innerHTML = "Submit";
+
+    const btnSchedules = document.createElement("button");
+    btnSchedules.ClassName = "btn btn-secondary";
+    btnSchedules.type = "button";
+    btnSchedules.innerHTML = "Schedules";
     btnCreate.addEventListener("click", function (event) {
         form.submit();
+        document.getElementById("kinosContainer").InnerHTML = "<p> Name:" + form.elements["Name"].value + "</p><p> Location:" + from.elements["Location"].value + "</p>";
+        document.getElementById("kinosContainer").appendChild(btnSchedules);
         form.reset();
     });
 
-    const inputName = document.Create.createElement("input");
+    const inputName = document.createElement("input");
     inputName.type = "text";
     inputName.name = "Name";
     inputName.placeholder = "Name";
     inputName.className = "form-control";
 
-    const inputLocation = document.Create.createElement("input");
+    const inputLocation = document.createElement("input");
     inputLocation.type = "text";
     inputLocation.name = "Location";
     inputLocation.placeholder = "Location";
     inputLocation.className = "form-control";
 
-    form.apendChild(btnCreate);
-    form.apendChild(inputName);
-    form.apendChild(inputLocation);
+    const inputCapacity = document.createElement("input");
+    inputCapacity.type = "number";
+    inputCapacity.name = "Capacity";
+    inputCapacity.placeholder = "Capacity";
+    inputCapacity.className = "form-control";
+
     document.getElementById("kinosContainer").appendChild(form);
+
+    form.appendChild(btnClose);
+    form.appendChild(inputName);
+    form.appendChild(inputLocation);
+    form.appendChild(inputCapacity);
+    form.appendChild(btnCreate);
 }
